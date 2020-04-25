94171d2d652d4b549ab1982324abca2f586b0a1f
brianbirir <brianbirir@gmail.com>
2020-04-25 18:03:34 +0300

    Add rgb library

--- src/rgb_led.ino
+++ src/rgb_led.ino
@@ -1,10 +1,20 @@
-int LED = D3
+#include "lib/rgb-controls/rgb-controls.h"
+using namespace RGBControls;
 
-void setup()
-{
-}
+
+int redPin = D2;
+int greenPin = D3;
+int bluePin = D4;
+
+Led led(redPin, greenPin, bluePin);
+
+Color red(255, 0, 0);
+Color blue(0, 0, 255);
+
+void setup(){}
 
 void loop()
 {
+  // Fade the led light between red and blue every 5 seconds
+  led.fade(red, blue, 5000);
 }
-
